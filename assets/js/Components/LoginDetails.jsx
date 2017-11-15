ed232226e890448ad93e93c7de72819490c8807d
Ryan Weaver <ryan@thatsquality.com>
2017-11-15 17:41:16 +0200

    Step: React: Simple {render}

--- assets/js/Components/LoginDetails.jsx
+++ assets/js/Components/LoginDetails.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default class LoginDetails extends React.Component {
     render() {
+        const name = 'Ryan';
+        
         return (
             <div>
-                I am the login details!
+                I am the login details! {name}
             </div>
         );
     }
