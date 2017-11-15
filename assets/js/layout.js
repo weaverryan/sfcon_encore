39a104a621807c171ee80746f110a51b6cc3b38f
Ryan Weaver <ryan@thatsquality.com>
2017-11-15 17:41:10 +0200

    Step: CSS: Bootstrap & FontAwesome

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,6 +2,8 @@
 
 const $ = require('jquery');
 require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
 require('../css/layout.css');
 
 $(document).ready(function() {
