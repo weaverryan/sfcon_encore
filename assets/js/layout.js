4f06d4aee95fa40536407f0f07731895f3586655
Ryan Weaver <ryan@thatsquality.com>
2017-11-15 17:41:23 +0200

    Step: Polyfill: Add polyfill

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,6 +5,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 import '../css/layout.scss';
+import 'core-js';
 
 $(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
